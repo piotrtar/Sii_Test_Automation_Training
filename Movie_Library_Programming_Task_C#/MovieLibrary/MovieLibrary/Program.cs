ac8ea19f415f470335a93ff5f0408dce1a1845dc
Piotr Tarczoń <ptarczon@SIIPOLSKA.PL>
2018-05-16 17:09:05 +0200

    Add Movie Library - console application to filter movies wrote in C#

---
+++ Movie_Library_Programming_Task_C#/MovieLibrary/MovieLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieLibrary.DAO;
+using MovieLibrary.VIEW;
+using MovieLibrary.Controller;
+using System.IO;
+
+namespace MovieLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dao Dao = new Dao();
+            View View = new View();
+            AppController AppController = new AppController(Dao, View);
+            Dao.LoadObjectsFromJsonFile();
+            AppController.HandleMenu();
+        }
+    }
+}
